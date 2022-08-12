a5e87cb0ec16c364a46afba75e46c1cf8f919696
Łukasz Kwiecień <lukaskwkw@gmail.com>
2022-08-12 11:42:29 +0200

    feat(body): Add convert html url paths to absolute if need functionality

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 use rand::{self, thread_rng, Rng};
+use serde::Deserialize;
 use serde_json::Value;
 
+#[derive(Deserialize)]
+struct Info {
+    username: Option<String>,
+}
+
 #[get("/")]
-async fn hello() -> impl Responder {
+async fn hello(info: web::Query<Info>) -> impl Responder {
+    if let Some(username) = info.username.as_ref() {
+        let query = format!("Welcome {}!", username);
+        
+    println!("query {}", query);
+    }
     let body: Value = reqwest::get("https://searx.space/data/instances.json")
         .await
         .unwrap()
         .json()
         .await
         .unwrap();
-    // println!("body = {:?}", body["instances"]["https://anon.sx/"]);
-    // println!("xd {}", body.as_object().unwrap().len());
     let instances = &body["instances"].as_object().unwrap();
     println!("instanes len {}", instances.len());
-    let best_grade_instances: Vec<_> = instances
+    let best_grade_instance_urls: Vec<_> = instances
         .iter()
         .filter_map(|k| {
             let grade: String = k.1["html"]["grade"]
@@ -23,40 +32,34 @@ async fn hello() -> impl Responder {
                 .unwrap_or_default()
                 .to_string();
             let network_type: String = k.1["network_type"].as_str().unwrap_or_default().to_string();
-            // println!("grade {}, n_type {}", grade, network_type);
             if ["C", "V"].contains(&&grade[..]) && network_type == "normal" {
                 Some(k.0)
             } else {
                 None
             }
-            // println!("k = {:#?}", k.0);
         })
         .collect();
-    println!("best grades len {}", best_grade_instances.len());
-    best_grade_instances.iter().for_each(|what| {
+    println!("best grades len {}", best_grade_instance_urls.len());
+    best_grade_instance_urls.iter().for_each(|what| {
         println!("{}", what);
     });
 
     let mut rng = thread_rng();
-    let random_plus: u32 = rng.gen_range(0..best_grade_instances.len() as u32);
-    let body = reqwest::get(
-        best_grade_instances
-            .get(random_plus as usize)
-            .unwrap()
-            .to_string(),
-    )
-    .await
-    .unwrap()
-    .text()
-    .await
-    .unwrap();
+    let random_plus: u32 = rng.gen_range(0..best_grade_instance_urls.len() as u32);
+    let url = best_grade_instance_urls
+        .get(random_plus as usize)
+        .unwrap()
+        .to_string();
+    let body = reqwest::get(&url).await.unwrap().text().await.unwrap();
+    let body = convert_html_urls_to_absolute(body, &url);
     HttpResponse::Ok().body(body)
     // HttpResponse::Ok().body(body)
 }
 
-fn convert_html_urls_to_absolute(body: &String, url: &String) -> String {
+fn convert_html_urls_to_absolute(body: String, url: &str) -> String {
     body.replace("href=\"/", &format!("href=\"{}", url))
-        .replace("src=\"/{}", &format!("href=\"{}", url))
+        .replace("src=\"/", &format!("src=\"{}", url))
+        .replace("\"/search", &format!("\"{}search", url))
 }
 
 #[actix_web::main]
@@ -66,4 +69,19 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
-// https://searx.space/data/instances.json
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn convert_html_urls_to_absolute_test() {
+        let body =
+            r#"<a href="/lola" /> <img src="/heheszki.jpg" /> <form action="/search"></form> "#
+                .to_string();
+        let after = convert_html_urls_to_absolute(body, "https://test.com/");
+        assert_eq!(
+            after,
+            r#"<a href="https://test.com/lola" /> <img src="https://test.com/heheszki.jpg" /> <form action="https://test.com/search"></form> "#
+        );
+    }
+}
