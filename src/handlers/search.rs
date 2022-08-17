ccd5569bb53ee2ab9b6d7156ccfbfe71d080f13a
Łukasz Kwiecień <lukaskwkw@gmail.com>
2022-08-17 09:47:34 +0200

    feat(get_body): Add get instance full body functionality

--- src/handlers/search.rs
+++ src/handlers/search.rs
@@ -1,21 +1,17 @@
+use crate::searx_client::SearxClient;
 use actix_web::{
-    web::{self, Data}, HttpResponse, Responder,
+    web::{self, Data},
+    HttpResponse, Responder,
 };
 use rand::{self, thread_rng, Rng};
 use serde::Deserialize;
-use crate::searx_client::SearxClient;
 
 #[derive(Deserialize)]
 pub struct Query {
     q: Option<String>,
 }
 
-pub async fn search(_info: web::Query<Query>, client: Data<SearxClient>) -> impl Responder {
-    // if let Some(username) = info.username.as_ref() {
-    //     let query = format!("Welcome {}!", username);
-    //
-    //     println!("query {}", query);
-    // }
+pub async fn search(params: web::Query<Query>, client: Data<SearxClient>) -> impl Responder {
     let body = client.get_instances().await;
     let instances = &body["instances"].as_object().unwrap();
     println!("instanes len {}", instances.len());
@@ -35,40 +31,20 @@ pub async fn search(_info: web::Query<Query>, client: Data<SearxClient>) -> impl
         })
         .collect();
     println!("best grades len {}", best_grade_instance_urls.len());
-    best_grade_instance_urls.iter().for_each(|what| {
-        println!("{}", what);
-    });
 
+    let url = get_random_instance_url(best_grade_instance_urls);
+    let body = client
+        .get_instance_search_body(&url, &params.q.clone().unwrap_or_default())
+        .await;
+    HttpResponse::Ok().body(body)
+}
+
+fn get_random_instance_url(best_grade_instance_urls: Vec<&String>) -> String {
     let mut rng = thread_rng();
     let random_plus: u32 = rng.gen_range(0..best_grade_instance_urls.len() as u32);
     let url = best_grade_instance_urls
         .get(random_plus as usize)
         .unwrap()
         .to_string();
-    let body = reqwest::get(&url).await.unwrap().text().await.unwrap();
-    let body = convert_html_urls_to_absolute(body, &url);
-    HttpResponse::Ok().body(body)
-    // HttpResponse::Ok().body(body)
-}
-
-fn convert_html_urls_to_absolute(body: String, url: &str) -> String {
-    body.replace("href=\"/", &format!("href=\"{}", url))
-        .replace("src=\"/", &format!("src=\"{}", url))
-        .replace("\"/search", &format!("\"{}search", url))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn convert_html_urls_to_absolute_test() {
-        let body =
-            r#"<a href="/lola" /> <img src="/heheszki.jpg" /> <form action="/search"></form> "#
-                .to_string();
-        let after = convert_html_urls_to_absolute(body, "https://test.com/");
-        assert_eq!(
-            after,
-            r#"<a href="https://test.com/lola" /> <img src="https://test.com/heheszki.jpg" /> <form action="https://test.com/search"></form> "#
-        );
-    }
+    url
 }
--- src/searx_client.rs
+++ src/searx_client.rs
@@ -1,27 +1,81 @@
-use reqwest::Client;
+use reqwest::{Client, Url};
 use serde_json::Value;
 
 #[derive(Debug)]
 pub struct SearxClient {
     http_client: Client,
-    base_url: String,
+    base_url: Url,
 }
 
 impl SearxClient {
     pub fn new(base_url: String) -> Self {
         Self {
-            base_url,
+            base_url: Url::parse(&base_url).unwrap(),
             http_client: Client::new(),
         }
     }
     pub async fn get_instances(&self) -> Value {
+        let url = Url::join(&self.base_url, "data/instances.json").unwrap();
         self.http_client
-            .get("https://searx.space/data/instances.json")
+            .get(url)
             .send()
             .await
             .unwrap()
             .json()
             .await
             .unwrap()
+    }
+    pub async fn get_instance_search_body(&self, instance_url: &str, query: &str) -> String {
+        let url = get_insance_search_url(instance_url, query);
+        let body = self
+            .http_client
+            .get(url)
+            .send()
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+        convert_html_urls_to_absolute(body, instance_url)
+    }
+}
+
+fn convert_html_urls_to_absolute(body: String, url: &str) -> String {
+    body.replace("href=\"/", &format!("href=\"{}", url))
+        .replace("src=\"/", &format!("src=\"{}", url))
+        .replace("\"/search", &format!("\"{}search", url))
+}
+
+fn get_insance_search_url(instance_url: &str, query: &str) -> Url {
+    let search_route = format!("/search?q={}", query);
+    let url = Url::parse(instance_url)
+        .unwrap()
+        .join(&search_route)
+        .unwrap();
+    println!("instance full url {url}");
+
+    url
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn convert_html_urls_to_absolute_test() {
+        let body =
+            r#"<a href="/lola" /> <img src="/heheszki.jpg" /> <form action="/search"></form> "#
+                .to_string();
+        let after = convert_html_urls_to_absolute(body, "https://test.com/");
+        assert_eq!(
+            after,
+            r#"<a href="https://test.com/lola" /> <img src="https://test.com/heheszki.jpg" /> <form action="https://test.com/search"></form> "#
+        );
+    }
+    #[test]
+    fn get_insance_search_url_test() {
+        let instance = "http://searx.jp/";
+        let query = "semaphore";
+        let expected_url = "http://searx.jp/search?q=semaphore";
+        let result = get_insance_search_url(instance, query).to_string();
+        assert_eq!(result, expected_url);
     }
 }
